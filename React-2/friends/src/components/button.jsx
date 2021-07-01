be3e1dbfc136f546b0125b973bb8325dcc3b1f0f
FacuRearte <Facundorearte13@gmail.com>
2021-06-30 23:55:48 -0300

    Proyecto finalizado

--- React-2/friends/src/components/button.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Button = styled.button`
-    padding: 0.25rem 0.75rem;
-    text-transform: uppercase;
-    font-weight: bold;
-`
-
-export default Button;
